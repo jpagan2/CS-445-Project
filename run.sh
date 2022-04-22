577084dee54571c7feefe2d87ff504da9b60fba6
jpagan2 <jpagan2@hawk.iit.edu>
2022-04-21 19:54:16 -0500

    updated run script

--- run.sh
+++ run.sh
@@ -2,3 +2,19 @@
 
 # Script to build and run "Buy Nothing RESTful API"
 
+
+#Install openjdk-17-jdk
+sudo apt update -y
+sudo apt install openjdk-17-jdk -y
+java --version
+
+#Install Maven
+wget https://dlcdn.apache.org/maven/maven-3/3.8.5/binaries/apache-maven-3.8.5-bin.tar.gz -P /tmp
+sudo tar xf /tmp/apache-maven-*.tar.gz -C /opt
+sudo ln -s /opt/apache-maven-3.8.5 /opt/maven
+
+#Install Vim
+sudo apt-get install vim -y
+
+#Install Postman
+sudo snap install postman
